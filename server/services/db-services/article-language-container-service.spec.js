040983e715055c5815afa35cdb80ece3e8ddc440
Eldar Khaitov <e.khaitov@andersenlab.com>
2018-06-26 19:25:48 +0300

    Added some test and code for article language container

--- server/services/db-services/article-language-container-service.spec.js
+++ server/services/db-services/article-language-container-service.spec.js
@@ -39,27 +39,100 @@ describe('Article Language Container Service', function () {
                 })
         });
 
-        it('should reject creating container if name is not specified', done => {});
+        it('should reject creating container if name is not specified', done => {
+            articleLanguageService.create()
+                .then(container => {
+                    done(container);
+                })
+                .catch(err => {
+                    expect(err.message).to.equal('Language container should have a name');
+                    done();
+                })
+        });
     });
 
     describe('UPDATE', () => {
-        it('should update container with given parameters', done => {});
+        it('should update container with given parameters', done => {
+            const name = 'new container name';
+            const ru = 'ru';
 
-        it('should reject updating container if name is not specified', done => {});
+            articleLanguageService.update(languageContainerId, {name, ru})
+                .then(container => {
+                    expect(container).to.deep.include({name, ru});
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                })
+        });
 
-        it('should reject updating container if id is not specified', done => {});
+        it('should reject updating container if body is empty', done => {
+            articleLanguageService.update(languageContainerId, {})
+                .then(() => {
+                    done(1);
+                })
+                .catch(err => {
+                    expect(err.message).to.equal('Container update should have body');
+                    done();
+                })
+        });
 
-        it('should reject updating container if a container with current id was not found', done => {});
+        it('should reject updating container if id is not specified', done => {
+            articleLanguageService.update(null, {})
+                .then(() => {
+                    done(1);
+                })
+                .catch(err => {
+                    expect(err.message).to.equal('Container update should have id');
+                    done();
+                })
+        });
+
+        it('should reject updating container if a container with current id was not found', done => {
+            articleLanguageService.update('5b3264eebc6a4001bc47bc6b', {name: 'tratata'})
+                .then(() => {
+                    done(1);
+                })
+                .catch(err => {
+                    expect(err.message).to.equal('Container with given id was not found');
+                    done();
+                })
+        });
     });
 
     describe('READ', () => {
-        it('should read container with specified id', done => {});
+        it('should read container with specified id', done => {
+            articleLanguageService.read(languageContainerId)
+                .then(container => {
+                    expect(container._id.toString()).to.equal(languageContainerId);
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                })
+        });
 
-        it('should read all the containers', done => {});
+        it('should read all the containers', done => {
+            articleLanguageService.read()
+                .then(containers => {
+                    expect(containers).to.be.an('array');
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                })
+        });
 
-        it('should reject reading container if id is not specified', done => {});
-
-        it('should reject reading container if a container with current id was not found', done => {});
+        it('should reject reading container if a container with current id was not found', done => {
+            articleLanguageService.read('5b3264eebc6a4001bc47bc6b')
+                .then(() => {
+                    done(1);
+                })
+                .catch(err => {
+                    expect(err.message).to.equal('Container with id 5b3264eebc6a4001bc47bc6b was not found');
+                    done();
+                })
+        });
     });
 
     describe('DELETE', () => {
@@ -74,8 +147,26 @@ describe('Article Language Container Service', function () {
                 })
         });
 
-        it('should reject deleting a container if id is not specified', done => {});
+        it('should reject deleting a container if id is not specified', done => {
+            articleLanguageService.remove()
+                .then(() => {
+                    done(1)
+                })
+                .catch(err => {
+                    expect(err.message).to.equal('Container delete should have an Id');
+                    done();
+                })
+        });
 
-        it('should reject deleting a container if a container with current id was not found', done => {});
+        it('should reject deleting a container if a container with current id was not found', done => {
+            articleLanguageService.remove('5b3264eebc6a4001bc47bc6b')
+                .then(() => {
+                    done(1)
+                })
+                .catch(err => {
+                    expect(err.message).to.equal('Container with id 5b3264eebc6a4001bc47bc6b was not found');
+                    done();
+                })
+        });
     });
 });
