063d776d10004f8d9155f32b9985c056ce248ad1
Eldar Khaitov <e.khaitov@andersenlab.com>
2018-06-26 10:34:49 +0300

    Added some test and code for article language container

--- server/services/db-services/article-language-container-service.spec.js
+++ server/services/db-services/article-language-container-service.spec.js
@@ -3,8 +3,7 @@ const expect = require('chai').expect;
 const mongoose = require('mongoose');
 const config = require('../../config');
 
-describe('Article Service', function () {
-    let articleId;
+describe('Article Language Container Service', function () {
     let languageContainerId;
 
     before(done => {
@@ -17,6 +16,7 @@ describe('Article Service', function () {
             })
     });
     after(done => {
+        mongoose.connection.db.dropCollection('articlelanguagecontainers');
         mongoose.disconnect()
             .then(() => {
                 done()
@@ -26,7 +26,56 @@ describe('Article Service', function () {
             })
     });
 
-    it('should create a new article language container', done => {
-        done(1)
+    describe('CREATE', () => {
+        it('should create container with given parameters', done => {
+            articleLanguageService.create('container')
+                .then(container => {
+                    expect(container.name).to.be.equal('container');
+                    languageContainerId = container.id;
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                })
+        });
+
+        it('should reject creating container if name is not specified', done => {});
+    });
+
+    describe('UPDATE', () => {
+        it('should update container with given parameters', done => {});
+
+        it('should reject updating container if name is not specified', done => {});
+
+        it('should reject updating container if id is not specified', done => {});
+
+        it('should reject updating container if a container with current id was not found', done => {});
+    });
+
+    describe('READ', () => {
+        it('should read container with specified id', done => {});
+
+        it('should read all the containers', done => {});
+
+        it('should reject reading container if id is not specified', done => {});
+
+        it('should reject reading container if a container with current id was not found', done => {});
+    });
+
+    describe('DELETE', () => {
+        it('should delete a container with given parameters as deleted', done => {
+            articleLanguageService.remove(languageContainerId)
+                .then(result => {
+                    expect(result.n).to.be.equal(1);
+                    done()
+                })
+                .catch(err => {
+                    done(err);
+                })
+        });
+
+        it('should reject deleting a container if id is not specified', done => {});
+
+        it('should reject deleting a container if a container with current id was not found', done => {});
     });
 });
--- server/services/services.spec.js
+++ server/services/services.spec.js
@@ -1,3 +1,4 @@
 require('./auth-service.spec');
 require('./file-service.spec');
 require('./db-services/article-service.spec');
+require('./db-services/article-language-container-service.spec');
