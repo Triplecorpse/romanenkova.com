50321147209c524e3b091dc19a3aa4fa3e5ab21f
Eldar Khaitov <eldar.khaitov@gmail.com>
2018-08-12 11:55:41 +0300

    Configured diploma model

--- server/models/diploma.ts
+++ server/models/diploma.ts
@@ -2,15 +2,9 @@ import * as mongoose from 'mongoose';
 const schema = new mongoose.Schema({
     picture: {type: String, required: true},
     header: {type: String, required: true},
-    body: {type: String, required: true},
-    isPublished: {type: Boolean, require: true},
+    receivedAt: {type: Date, required: true},
     language: {type: String, require: true},
-    createdAt: {type: Date, require: true, default: new Date()},
-    updatedAt: {type: Date},
-    deletedAt: {type: Date},
-    isDeleted: {type: Boolean, default: false},
-    url: {type: String, required: true, lowercase: true},
     entityId: {type: String, required: true}
-});
+}, {timestamps: {createdAt: 'createdAt', updatedAt: 'updatedAt'}});
 
-export const model = mongoose.model('Article', schema);
+export const model = mongoose.model('Diploma', schema);
