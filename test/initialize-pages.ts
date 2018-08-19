565e58c3851291a7c978f8ea238a850d10274d94
Eldar Khaitov <e.khaitov@andersenlab.com>
2018-08-19 23:55:21 +0300

    Changed modal model

--- test/initialize-pages.ts
+++ test/initialize-pages.ts
@@ -286,66 +286,67 @@ mongoose.connect(process.env.MONGODB_URI as string)
         pagesToPush.push({
             entityId: '[modal] appointment',
             header: 'Make an appointment',
-            pageData: [
-                {name: 'First and last names'},
-                {phone: 'Phone'},
-                {email: 'Email'},
-                {date: 'Date'},
-                {time: 'Time'},
-                {service: 'Service'},
-                {message: 'Message'},
-                {checkbox: 'Mark this checkbox'},
-                {submit: 'OK'},
-                {explanation: 'No personal data entered in this form is stored on the site or transmitted to third parties for promotional purposes. Contact information is used only for contact with you and is saved only in my personal mail box.'}
-            ],
+            pageData: {
+                name: 'First and last names',
+                phone: 'Phone',
+                email: 'Email',
+                date: 'Date',
+                time: 'Time',
+                service: 'Service',
+                message: 'Message',
+                checkbox: 'Mark this checkbox',
+                submit: 'OK',
+                explanation: 'No personal data entered in this form is stored on the site or transmitted to third parties for promotional purposes. Contact information is used only for contact with you and is saved only in my personal mail box.'
+            },
             language: 'en'
         }, {
             entityId: '[modal] appointment',
             header: 'Записаться на приём',
-            pageData: [
-                {name: 'Имя и фамилия'},
-                {phone: 'Телефон'},
-                {email: 'Email'},
-                {date: 'Дата'},
-                {time: 'Время'},
-                {service: 'Вид консультации'},
-                {message: 'Сообщение'},
-                {checkbox: 'С правилами сайта ознакомлен'},
-                {submit: 'Записаться'},
-                {explanation: 'Никакие личные данные, введённые в этой форме, не сохраняются на сайте и не передаются третьим лицам в рекламных целях. Контактная информация используется только для контакта с вами и сохраняется только в моём личном почтовом ящике.'}
-            ],
+            pageData: {
+                name: 'Имя и фамилия',
+                phone: 'Телефон',
+                email: 'Email',
+                date: 'Дата',
+                time: 'Время',
+                service: 'Вид консультации',
+                message: 'Сообщение',
+                checkbox: 'С правилами сайта ознакомлен',
+                submit: 'Записаться',
+                explanation: 'Никакие личные данные, введённые в этой форме, не сохраняются на сайте и не передаются третьим лицам в рекламных целях. Контактная информация используется только для контакта с вами и сохраняется только в моём личном почтовом ящике.'
+    },
             language: 'ru'
         }, {
             entityId: '[modal] appointment',
             header: 'Записатися на прийом',
-            pageData: [
-                {name: 'Ім\'я та прізвище'},
-                {phone: 'Телефон'},
-                {email: 'Email'},
-                {date: 'Дата'},
-                {time: 'Час'},
-                {service: 'Сервіс'},
-                {message: 'Повідомлення'},
-                {checkbox: 'З правилами ознайомлений'},
-                {submit: 'Записатися'},
-                {explanation: 'Ніякі особисті дані, які були введені у цій формі, не зберігаються на сайті і не передаються третім особам у рекламних цілях. Контактна інформація використовується тільки для контакта з вамиі зберігається тільки у моїй особистій поштовій скринці.'}
-            ],
+            pageData: {
+                name: 'Ім\'я та прізвище',
+                phone: 'Телефон',
+                email: 'Email',
+                date: 'Дата',
+                time: 'Час',
+                service: 'Сервіс',
+                message: 'Повідомлення',
+                checkbox: 'З правилами ознайомлений',
+                submit: 'Записатися',
+                explanation: 'Ніякі особисті дані, які були введені у цій формі, не зберігаються на сайті і не передаються третім особам у рекламних цілях. Контактна інформація використовується тільки для контакта з вамиі зберігається тільки у моїй особистій поштовій скринці.'
+            },
             language: 'uk'
         }, {
             entityId: '[modal] appointment',
             header: 'Prendre rendez-vous',
-            pageData: [
-                {name: 'Nom et prénom'},
-                {phone: 'Numéro de téléphone'},
-                {email: 'Email'},
-                {date: 'Date'},
-                {time: 'Heure'},
-                {service: 'Type de consultation'},
-                {message: 'Votre message'},
-                {checkbox: 'Je lis les règles du site'},
-                {submit: 'S\'inscrire'},
-                {explanation: 'Aucune donnée personnelle saisie dans ce formulaire n\'est stockée sur le site ou transmise à des tiers à des fins promotionnelles. Les informations de contact ne sont utilisées que pour vous contacter et sont enregistrées uniquement dans ma boîte aux lettres personnelle.'}
-            ],
+            pageData:
+            {
+                name: 'Nom et prénom',
+                phone: 'Numéro de téléphone',
+                email: 'Email',
+                date: 'Date',
+                time: 'Heure',
+                service: 'Type de consultation',
+                message: 'Votre message',
+                checkbox: 'Je lis les règles du site',
+                submit: 'S\'inscrire',
+                explanation: 'Aucune donnée personnelle saisie dans ce formulaire n\'est stockée sur le site ou transmise à des tiers à des fins promotionnelles. Les informations de contact ne sont utilisées que pour vous contacter et sont enregistrées uniquement dans ma boîte aux lettres personnelle.'
+            },
             language: 'fr'
         });
 
