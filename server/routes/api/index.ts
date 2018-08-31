7c539de97b222f2a2f480696c79e255f2b925713
Eldar Khaitov <e.khaitov@andersenlab.com>
2018-09-01 01:04:06 +0300

    Refactored error messages

--- server/routes/api/index.ts
+++ server/routes/api/index.ts
@@ -86,15 +86,15 @@ router.post('/appointment', (req: IRequest, res: Response) => {
     const appointment: IAppointment = req.body as IAppointment;
 
     if (!appointment.name && (!appointment.email || appointment.phone || appointment.message)) {
-        return res.status(400).json({err: {name: 'Please enter your name', contact: 'Please enter at least one of your contact data or message'}})
+        return res.status(400).json({name: 'Please enter your name', contact: 'Please enter at least one of your contact data or message'})
     }
 
     if (!appointment.name) {
-        return res.status(400).json({err: {name: 'Please enter your name'}})
+        return res.status(400).json({name: 'Please enter your name'})
     }
 
     if (!appointment.email || appointment.phone || appointment.message) {
-        return res.status(400).json({err: {contact: 'Please enter at least one of your contact data or message'}});
+        return res.status(400).json({contact: 'Please enter at least one of your contact data or message'});
     }
 
     validateRecaptcha(appointment.recaptcha)
