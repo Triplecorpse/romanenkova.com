414bf06cb324a358b10839bbea74a0763151393c
Eldar Khaitov <e.khaitov@andersenlab.com>
2019-01-17 19:09:54 +0200

    Completed SSR

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,30 @@
 import express = require('express');
 import bodyParser = require('body-parser');
 import routes from './routes';
+import {ngExpressEngine} from "@nguniversal/express-engine";
+import {provideModuleMap} from "@nguniversal/module-map-ngfactory-loader";
+import {enableProdMode} from "@angular/core";
+import {join} from "path";
+import {readFileSync} from "fs";
 
 export const app = express();
 
+enableProdMode();
+
+const DIST_FOLDER = join(process.cwd(), 'front');
+
+const template = readFileSync(join(DIST_FOLDER, 'index.html')).toString();
+const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./../front/main');
+
+app.engine('html', ngExpressEngine({
+    bootstrap: AppServerModuleNgFactory,
+    providers: [
+        provideModuleMap(LAZY_MODULE_MAP)
+    ]
+}));
+
+app.set('view engine', 'html');
+app.set('views', join(DIST_FOLDER));
+
 app.use('/', routes);
 app.use(bodyParser.json({type: 'application/json'}));
-
