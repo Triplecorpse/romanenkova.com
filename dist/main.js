c698410bbba2096eb967e56910ad34ff0161465e
Eldar Khaitov <e.khaitov@andersenlab.com>
2019-02-05 15:16:53 +0200

    Updated api links

--- dist/main.js
+++ dist/main.js
@@ -4609,8 +4609,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.environment = {
     production: true,
     api: 'https://www.romanenkova.com/api/',
-    // apiv2: 'https://www.romanenkova.com/api/v2/',
-    apiv2: 'http://localhost:3000/api/v2/',
+    apiv2: 'https://www.romanenkova.com/api/v2/',
     recaptcha: '6LdLOY0UAAAAAPrMEPErFr4JKRqtOnQQ5tKDFTRk'
 };
 
