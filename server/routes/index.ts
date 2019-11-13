9657a4f67bbf2ad36d8eb9154d428eab9a2e0c51
Eldar Khaitov <ext.ekhaitov@tcsbank.ru>
2019-11-13 18:59:22 +0200

    WIP setting up

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -6,6 +6,7 @@ import {NextFunction} from 'express';
 import {languages} from '../const/const';
 import {join} from 'path';
 import {TLanguage} from '../../_interface/types';
+import * as path from 'path';
 
 const cookieParser = require('cookie-parser');
 const parseAcceptLanguage = require('parse-accept-language');
@@ -19,18 +20,28 @@ router.use('*', (req: IRequest, res: Response, next: NextFunction) => {
     const languageObj = acceptedLangs.find((lang: any) =>
         languages.find((acceptable: string) => lang.language === acceptable)
     );
-    const isQa = req.hostname.includes('staging') || req.hostname.includes('qa');
+    const isQa = req.hostname.includes('staging') || req.hostname.includes('qa') || req.hostname.includes('localhost');
 
-    if (isQa && !req.cookies.allowQa && !req.query.secret !== 'allowQa') {
-        res.status(404).set('Content-Type', 'text/html').send('./front/assets/run-staging.html');
+    if (isQa && !req.cookies.allowQa && !req.query.secret) {
+        res.sendFile(path.join(__dirname + '../../../front/assets/run-staging.html'));
+
         return;
-    } else if(isQa && req.query.secret === 'allowQa') {
-        res.cookies.allowQa = 1;
     }
 
     req.language = req.query.lang || req.cookies.lang || (languageObj ? languageObj.language : 'en');
     req.isLocalhost = req.hostname.includes('localhost');
+
+
+
     next();
+});
+
+router.get('/qa', (req: IRequest, res: Response) => {
+    if (req.query.secret === 'allowQa') {
+        res.cookie('allowQa', 1).send();
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 router.get('/:lang?/:page?/:entity?', (req: IRequest, res: Response, next: NextFunction) => {
