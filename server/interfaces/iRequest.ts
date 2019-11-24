9f91c298937899cfeab92a54a19a78d11f8ea513
Eldar Khaitov <e.khaitov@andersenlab.com>
2019-11-24 12:45:06 +0200

    fixed qa

--- server/interfaces/iRequest.ts
+++ server/interfaces/iRequest.ts
@@ -6,4 +6,5 @@ export default interface IRequest extends Request {
     files?: any;
     isTokenValid?: boolean;
     isLocalhost?: boolean;
+    isQa?: boolean;
 }
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -15,29 +15,15 @@ const router = express.Router();
 
 router.use(cookieParser('secretthings'));
 
-router.get('/qa', (req: IRequest, res: Response) => {
-  if (req.query.secret === 'allowQa') {
-    res.cookie('allowQa', 1).json({ok: true});
-  } else {
-    res.status(403).json({ok: false});
-  }
-});
-
 router.use('*', (req: IRequest, res: Response, next: NextFunction) => {
     const acceptedLangs = parseAcceptLanguage(req);
     const languageObj = acceptedLangs.find((lang: any) =>
         languages.find((acceptable: string) => lang.language === acceptable)
     );
-    const isQa = req.hostname.includes('staging') || req.hostname.includes('qa') || req.hostname.includes('localhost');
 
-    if (isQa && !req.cookies.allowQa) {
-        res.sendFile(path.join(__dirname + '../../../front/assets/run-staging.html'));
-
-        return;
-    }
-
-    req.language = req.query.lang || req.cookies.lang || (languageObj ? languageObj.language : 'en');
+    req.isQa = req.hostname.includes('staging') || req.hostname.includes('qa') || req.hostname.includes('localhost');
     req.isLocalhost = req.hostname.includes('localhost');
+    req.language = req.query.lang || req.cookies.lang || (languageObj ? languageObj.language : 'en');
 
     next();
 });
@@ -48,20 +34,34 @@ router.get('/:lang?/:page?/:entity?', (req: IRequest, res: Response, next: NextF
         return next();
     }
 
+    if (req.isQa && !req.cookies.allowQa) {
+      res.sendFile(path.join(__dirname + '../../../front/assets/run-staging.html'));
+
+      return;
+    }
+
     req.language = req.params.lang as TLanguage || req.language;
 
     const DIST_FOLDER = join(process.cwd(), 'front');
 
     res.render(join(DIST_FOLDER, 'index.html'), {req});
 });
+
 router.get('/uptime', (req: Request, res: Response, next: NextFunction) => {
     res.send(builtOn.toString());
     next();
 });
 
+router.get('/qa', (req: IRequest, res: Response) => {
+  if (req.query.secret === 'allowQa') {
+    res.cookie('allowQa', 1).json({ok: true});
+  } else {
+    res.status(403).json({ok: false});
+  }
+});
+
 router.use(express.static('./front'));
 router.use(express.static('./static'));
-
 router.use('/api/v2', apiv2);
 
 // router.get('*', (req: IRequest, res: Response) => {
