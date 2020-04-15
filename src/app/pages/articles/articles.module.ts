27cec0e362cfd634965e54db521f723255627fa8
Eldar Khaitov <e.khaitov@andersenlab.com>
2020-04-15 17:05:17 +0300

    added articles scroll to top

--- src/app/pages/articles/articles.module.ts
+++ src/app/pages/articles/articles.module.ts
@@ -3,14 +3,16 @@ import {CommonModule} from '@angular/common';
 import {ArticlesComponent} from './components/articles/articles.component';
 import {RouterModule} from '@angular/router';
 import {ArticleComponent} from '../../components/article/article.component';
+import {ScrollToModule} from '@nicky-lenaers/ngx-scroll-to';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild([
-      {path: '**', component: ArticlesComponent}
-    ])
-  ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild([
+            {path: '**', component: ArticlesComponent}
+        ]),
+        ScrollToModule
+    ],
   declarations: [ArticlesComponent, ArticleComponent]
 })
 export class ArticlesModule {
